53142b63dc4458932b838b90721881ab0f68718f
wgeboers <wesleygeboers@outlook.com>
2022-10-06 18:19:59 +0200

    Initial commit

---
+++ src/main/java/com/example/sharemobility/ShareMobilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.sharemobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMobilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareMobilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sharemobility/ShareMobilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sharemobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareMobilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
