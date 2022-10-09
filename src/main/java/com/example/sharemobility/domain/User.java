5524b2fc65584f686b97c782a293ce3bb8642271
MarcelForman <91687704+MarcelForman@users.noreply.github.com>
2022-10-09 18:41:08 +0200

    Update User.java
    abstract user class

--- src/main/java/com/example/sharemobility/domain/User.java
+++ src/main/java/com/example/sharemobility/domain/User.java
@@ -1,4 +1,31 @@
 package com.example.sharemobility.domain;
 
-public class User {
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected Long id;
+    protected String username;
+    protected String password;
+    protected String firstname;
+    protected String lastname;
+    protected String address;
+
+    public User(String username, String password, String firstname, String lastname, String address) {
+        this.username = username;
+        this.password = password;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+    }
 }
