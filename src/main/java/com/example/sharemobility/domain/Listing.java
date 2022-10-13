ec6c305072f50de91ef1906c88cff0f943133378
wgeboers <wesleygeboers@outlook.com>
2022-10-13 08:38:22 +0200

    Create Listing.java

---
+++ src/main/java/com/example/sharemobility/domain/Listing.java
@@ -0,0 +1,31 @@
+package com.example.sharemobility.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Listing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private LocalDateTime startDateRent;
+    private LocalDateTime endDateRent;
+    private int distance;
+    private int accelerationStart;
+    private int accelerationEnd;
+
+    public Listing() {
+
+    }
+
+    public int calculateBonuspoints(){
+
+        return 0;
+    }
+}
